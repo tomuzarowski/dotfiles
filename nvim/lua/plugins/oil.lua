b4e3a1cb955fffb31cba44435ade2cf741eaf32b
Tomek Uzarowski <t.uzarowski@empressia.pl>
2023-11-27 13:43:51 +0100

    feat: config oil to open in float window

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,11 +1,13 @@
 return {
 	"stevearc/oil.nvim",
-	opts = {},
-	-- Optional dependencies
-	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("oil").setup({
-			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
+			float = {
+				border = "rounded",
+				padding = 7,
+			},
+			vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" }),
 		})
 	end,
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 }

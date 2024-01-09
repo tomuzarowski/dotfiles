8b6b5574faf2f42d401ffb5edf2004e84e69c67a
Tomek <tomek.uzarowski@gmail.com>
2024-01-09 18:48:32 +0100

    feat: disable neotree

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -27,9 +27,10 @@ nnoremap("<leader>q", "<cmd>q<cr>", { silent = false })
 nnoremap("<leader>z", "<cmd>wq<cr>", { silent = false })
 
 -- Map neo-tree to <leader>e
-nnoremap("<leader>E", "<cmd>Neotree toggle<cr>")
+-- nnoremap("<leader>e", "<cmd>Neotree toggle<cr>")
+
 -- Focus on current buffer in neo-tree
-nnoremap("<leader>e", "<cmd>Neotree reveal<cr>")
+-- nnoremap("<leader>E", "<cmd>Neotree reveal<cr>")
 
 -- Center buffer while navigating
 nnoremap("<C-u>", "<C-u>zz")

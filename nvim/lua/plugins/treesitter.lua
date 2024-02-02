f5bafbb34825c27cf9274e4b27814094f914039b
Tomek Uzarowski <t.uzarowski@empressia.pl>
2024-02-02 09:55:22 +0100

    fix: update treesitter when opening file

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,7 @@
 return {
-
 	{
 		"nvim-treesitter/nvim-treesitter",
-		build = function()
-			require("nvim-treesitter.install").update({ with_sync = true })
-		end,
+		build = ":TSUpdate",
 		event = { "BufEnter" },
 		dependencies = {
 			-- Additional text objects for treesitter

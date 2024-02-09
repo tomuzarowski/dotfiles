131f9c58881d428037aa6f0eed01d1c15c511cd2
Tomek Uzarowski <t.uzarowski@empressia.pl>
2024-02-09 09:43:40 +0100

    feat: set 4 spaces indent and line wrapping

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -3,13 +3,13 @@ vim.opt.nu = true
 vim.opt.rnu = true
 
 -- Set tabs to 2 spaces
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
 -- Enable auto indenting and set it to spaces
 vim.opt.smartindent = true
-vim.opt.shiftwidth = 2
+vim.opt.shiftwidth = 4
 
 -- Enable smart indenting (see https://stackoverflow.com/questions/1204149/smart-wrap-in-vim)
 vim.opt.breakindent = true
@@ -19,7 +19,7 @@ vim.opt.incsearch = true
 vim.opt.hlsearch = true
 
 -- Disable text wrap
-vim.opt.wrap = false
+vim.opt.wrap = true
 
 -- Set leader key to space
 vim.g.mapleader = " "

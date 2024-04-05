fb14badee31f9ffcd785891ec3a1a1be4ffa86cf
tomekuzarowski <t.uzarowski@empressia.pl>
2024-04-05 13:53:04 +0200

    feat: change theme to cattpuccin

--- nvim/lua/plugins/color_scheme.lua
+++ nvim/lua/plugins/color_scheme.lua
@@ -17,51 +17,51 @@ return {
 	-- 		})
 	-- 	end,
 	-- },
-	{
-		"navarasu/onedark.nvim",
-		config = function()
-			require("onedark").setup({
-				style = "deep",
-				highlights = {
-					["@type"] = { fmt = "none" },
-					["@constructor"] = { fmt = "none" },
-				},
-			})
-			require("onedark").load()
-		end,
-	},
 	-- {
-	-- 	"catppuccin/nvim",
+	-- 	"navarasu/onedark.nvim",
 	-- 	config = function()
-	-- 		require("catppuccin").setup({
-	-- 			integrations = {
-	-- 				cmp = true,
-	-- 				gitsigns = true,
-	-- 				harpoon = true,
-	-- 				illuminate = true,
-	-- 				indent_blankline = {
-	-- 					enabled = false,
-	-- 					scope_color = "sapphire",
-	-- 					colored_indent_levels = false,
-	-- 				},
-	-- 				mason = true,
-	-- 				native_lsp = { enabled = true },
-	-- 				notify = true,
-	-- 				nvimtree = true,
-	-- 				neotree = true,
-	-- 				symbols_outline = true,
-	-- 				telescope = true,
-	-- 				treesitter = true,
-	-- 				treesitter_context = true,
+	-- 		require("onedark").setup({
+	-- 			style = "deep",
+	-- 			highlights = {
+	-- 				["@type"] = { fmt = "none" },
+	-- 				["@constructor"] = { fmt = "none" },
 	-- 			},
 	-- 		})
-	--
-	-- 		vim.cmd.colorscheme("catppuccin-macchiato")
-	--
-	-- 		-- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
-	-- 		for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
-	-- 			vim.api.nvim_set_hl(0, group, {})
-	-- 		end
+	-- 		require("onedark").load()
 	-- 	end,
 	-- },
+	{
+		"catppuccin/nvim",
+		config = function()
+			require("catppuccin").setup({
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					harpoon = true,
+					illuminate = true,
+					indent_blankline = {
+						enabled = false,
+						scope_color = "sapphire",
+						colored_indent_levels = false,
+					},
+					mason = true,
+					native_lsp = { enabled = true },
+					notify = true,
+					nvimtree = true,
+					neotree = true,
+					symbols_outline = true,
+					telescope = true,
+					treesitter = true,
+					treesitter_context = true,
+				},
+			})
+
+			vim.cmd.colorscheme("catppuccin-macchiato")
+
+			-- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
+			for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+				vim.api.nvim_set_hl(0, group, {})
+			end
+		end,
+	},
 }

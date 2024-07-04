2cd6cec6dac24bf43af0565d8674fbe77c70166e
tomekuzarowski <t.uzarowski@empressia.pl>
2024-07-04 10:30:14 +0200

    feat: add a new color scheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -31,39 +31,56 @@ return {
 	-- 	end,
 	-- },
 	{
-		"catppuccin/nvim",
+		"olivercederborg/poimandres.nvim",
+		lazy = false,
 		priority = 1000,
 		config = function()
-			require("catppuccin").setup({
-				-- transparent_background = true,
-				integrations = {
-					cmp = true,
-					gitsigns = true,
-					harpoon = true,
-					illuminate = true,
-					indent_blankline = {
-						enabled = false,
-						scope_color = "sapphire",
-						colored_indent_levels = false,
-					},
-					mason = true,
-					native_lsp = { enabled = true },
-					notify = true,
-					nvimtree = true,
-					neotree = true,
-					symbols_outline = true,
-					telescope = true,
-					treesitter = true,
-					treesitter_context = true,
-				},
+			require("poimandres").setup({
+				-- leave this setup function empty for default config
+				-- or refer to the configuration section
+				-- for configuration options
 			})
+		end,
 
-			vim.cmd.colorscheme("catppuccin-mocha")
-
-			-- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
-			for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
-				vim.api.nvim_set_hl(0, group, {})
-			end
+		-- optionally set the colorscheme within lazy config
+		init = function()
+			vim.cmd("colorscheme poimandres")
 		end,
 	},
+	-- {
+	-- 	"catppuccin/nvim",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("catppuccin").setup({
+	-- 			-- transparent_background = true,
+	-- 			integrations = {
+	-- 				cmp = true,
+	-- 				gitsigns = true,
+	-- 				harpoon = true,
+	-- 				illuminate = true,
+	-- 				indent_blankline = {
+	-- 					enabled = false,
+	-- 					scope_color = "sapphire",
+	-- 					colored_indent_levels = false,
+	-- 				},
+	-- 				mason = true,
+	-- 				native_lsp = { enabled = true },
+	-- 				notify = true,
+	-- 				nvimtree = true,
+	-- 				neotree = true,
+	-- 				symbols_outline = true,
+	-- 				telescope = true,
+	-- 				treesitter = true,
+	-- 				treesitter_context = true,
+	-- 			},
+	-- 		})
+	--
+	-- 		vim.cmd.colorscheme("catppuccin-mocha")
+	--
+	-- 		-- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
+	-- 		for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+	-- 			vim.api.nvim_set_hl(0, group, {})
+	-- 		end
+	-- 	end,
+	-- },
 }

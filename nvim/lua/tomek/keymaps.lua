68424bc998719aed36d6ac6dad194b9694a36190
tomekuzarowski <t.uzarowski@empressia.pl>
2025-01-02 09:03:26 +0100

    fix shortcut description

--- nvim/lua/tomek/keymaps.lua
+++ nvim/lua/tomek/keymaps.lua
@@ -5,7 +5,7 @@ local keymap = vim.keymap
 keymap.set("n", "<CR>", "<cmd>nohl<CR>", { desc = "Clear search highlights" })
 
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 

f3284800eadb47585656ea262323c44658ea3187
tomekuzarowski <t.uzarowski@empressia.pl>
2025-11-04 14:22:03 +0100

    feat: add options for oil

--- nvim/lua/user/plugins/oil.lua
+++ nvim/lua/user/plugins/oil.lua
@@ -1,9 +1,15 @@
 return {
-  'stevearc/oil.nvim',
-  opts = {},
-  dependencies = { { "nvim-tree/nvim-web-devicons" }, { "nvim-mini/mini.icons", opts = {} } },
-  lazy = false,
-  keys = {
-      { '-', ':Oil<CR>', desc = 'Open parent directory' },
-  }
+	"stevearc/oil.nvim",
+	opts = {
+		-- Configuration for the floating window in oil.open_float
+		float = {
+			max_width = 0.9,
+			max_height = 0.8,
+		},
+	},
+	dependencies = { { "nvim-tree/nvim-web-devicons" }, { "nvim-mini/mini.icons", opts = {} } },
+	lazy = false,
+	keys = {
+		{ "-", ":Oil --float<CR>", desc = "Open parent directory" },
+	},
 }

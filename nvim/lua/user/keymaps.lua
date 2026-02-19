e99bb180188d3a30a3b1d25f292c9cc359d7b20d
tomekuzarowski <t.uzarowski@empressia.pl>
2026-02-19 19:35:02 +0100

    refactor: use lower case q + leader to close all buffers

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,43 +1,43 @@
 -- Change leader key
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Clear search highlighting.
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Close all open buffers.
-vim.keymap.set('n', '<leader>Q', ':bufdo bdelete<CR>', { desc = 'Delete all buffers' })
+vim.keymap.set("n", "<leader>q", ":bufdo bdelete<CR>", { desc = "Delete all buffers" })
 
 -- Diagnostics.
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [d]iagnostic' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [d]iagnostic' })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [d]iagnostic" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [d]iagnostic" })
 
 -- Maintain the cursor position when yanking a visual selection.
 -- http://ddrscott.github.io/blog/2016/yank-without-jank/
-vim.keymap.set('v', 'y', 'myy`y')
-vim.keymap.set('v', 'Y', 'myY`y')
+vim.keymap.set("v", "y", "myy`y")
+vim.keymap.set("v", "Y", "myY`y")
 
 -- When text is wrapped, move by terminal rows, not lines, unless a count is provided.
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
 
 -- Paste replace visual selection without copying it.
-vim.keymap.set('v', 'p', '"_dP')
+vim.keymap.set("v", "p", '"_dP')
 
 -- Disable annoying command line thing.
-vim.keymap.set('n', 'q:', ':q<CR>')
+vim.keymap.set("n", "q:", ":q<CR>")
 
 -- Resize with arrows.
-vim.keymap.set('n', '<C-Up>', ':resize +2<CR>')
-vim.keymap.set('n', '<C-Down>', ':resize -2<CR>')
-vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>')
-vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>')
+vim.keymap.set("n", "<C-Up>", ":resize +2<CR>")
+vim.keymap.set("n", "<C-Down>", ":resize -2<CR>")
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>")
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
 
-vim.keymap.set('n', '<leader>w', '<cmd>write<CR>', { desc = 'Save file' })
+vim.keymap.set("n", "<leader>w", "<cmd>write<CR>", { desc = "Save file" })
 
 -- Split windows.
-vim.keymap.set('n', '<leader>vs', '<cmd>vsplit<CR>', { desc = 'Split window vertically' })
-vim.keymap.set('n', '<leader>vh', '<cmd>split<CR>', { desc = 'Split window horizontally' })
+vim.keymap.set("n", "<leader>vs", "<cmd>vsplit<CR>", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>vh", "<cmd>split<CR>", { desc = "Split window horizontally" })
 
 -- Move text up and down
 -- vim.keymap.set('i', '<A-j>', '<Esc>:move .+1<CR>==gi')

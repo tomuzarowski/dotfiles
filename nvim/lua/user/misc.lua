15b085ffb0ad8786200fb683712c0cff5d211675
tomekuzarowski <t.uzarowski@empressia.pl>
2026-03-31 08:48:08 +0200

    feat: wrap text in md and txt files

--- nvim/lua/user/misc.lua
+++ nvim/lua/user/misc.lua
@@ -1,3 +1,12 @@
+vim.api.nvim_create_autocmd('FileType', {
+  desc = 'enable wrap for markdown and text files',
+  group = vim.api.nvim_create_augroup('wrap-text-files', { clear = true }),
+  pattern = { 'markdown', 'text' },
+  callback = function()
+    vim.opt_local.wrap = true
+  end,
+})
+
 vim.api.nvim_create_autocmd('textyankpost', {
   desc = 'highlight when yanking text',
   group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),

603e578bcec74dab3fcc11a36db056cd92e85123
tomekuzarowski <t.uzarowski@empressia.pl>
2026-04-07 15:35:14 +0200

    fix: update treesitter for neovim 0.12

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -2,14 +2,36 @@
 
 return {
   'nvim-treesitter/nvim-treesitter',
-  event = 'VeryLazy',
-  build = function()
-    require('nvim-treesitter.install').update({ with_sync = true })
-  end,
+  branch = 'main',
+  lazy = false,
+  build = ':TSUpdate',
   dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
+    {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      branch = 'main',
+      config = function()
+        require('nvim-treesitter-textobjects').setup({
+          select = {
+            lookahead = true,
+          },
+        })
+
+        local select = require('nvim-treesitter-textobjects.select')
+        vim.keymap.set({ 'x', 'o' }, 'if', function()
+          select.select_textobject('@function.inner', 'textobjects')
+        end)
+        vim.keymap.set({ 'x', 'o' }, 'af', function()
+          select.select_textobject('@function.outer', 'textobjects')
+        end)
+        vim.keymap.set({ 'x', 'o' }, 'ia', function()
+          select.select_textobject('@parameter.inner', 'textobjects')
+        end)
+        vim.keymap.set({ 'x', 'o' }, 'aa', function()
+          select.select_textobject('@parameter.outer', 'textobjects')
+        end)
+      end,
+    },
   },
-  main = 'nvim-treesitter.configs',
   opts = {
     ensure_installed = {
       'bash',
@@ -30,14 +52,17 @@ return {
       'jsonc',
       'lua',
       'markdown',
+      'markdown_inline',
       'php',
       'php_only',
       'phpdoc',
+      'query',
       'regex',
       'sql',
       'svelte',
       'typescript',
       'vim',
+      'vimdoc',
       'vue',
       'xml',
       'yaml',
@@ -50,37 +75,14 @@ return {
       enable = true,
       disable = { "yaml" }
     },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ['if'] = '@function.inner',
-          ['af'] = '@function.outer',
-          ['ia'] = '@parameter.inner',
-          ['aa'] = '@parameter.outer',
-        },
-      },
-    },
   },
   config = function (_, opts)
-    local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-
-    parser_config.blade = {
-      install_info = {
-        url = "https://github.com/EmranMR/tree-sitter-blade",
-        files = {"src/parser.c"},
-        branch = "main",
-      },
-      filetype = "blade"
-    }
-
     vim.filetype.add({
       pattern = {
         ['.*%.blade%.php'] = 'blade',
       },
     })
 
-    require('nvim-treesitter.configs').setup(opts)
+    require('nvim-treesitter').setup(opts)
   end,
 }
